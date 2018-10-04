0dc6b6826acc6f9d629de202655c1ad6182b33df
Keenal <kms118@students.uwf.edu>
2018-10-04 17:10:21 -0500

    finish baserepo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TUTORized.Repository;
@@ -13,7 +10,7 @@ namespace TUTORized.Controllers
         public async Task<IActionResult> Index() 
         {
             BaseRepository br = new BaseRepository();
-            await br.testConnection();
+            await br.TestConnection();
             return View();
         }
 
--- Repository/BaseRepository.cs
+++ Repository/BaseRepository.cs
@@ -10,16 +10,19 @@ namespace TUTORized.Repository
 {
     public class BaseRepository
     {
-
         private IDbConnection Connection => new SqlConnection(ConnectionString);
 
         private string ConnectionString = "Server=tcp:tma.database.windows.net,1433;Initial Catalog=TMA;Persist Security Info=False;User ID=tmadev;Password=!qa@ws3ed;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
+        //default constructor
         public BaseRepository()
         {
 
         }
-        public async Task testConnection()
+
+        //test connection
+        
+        public async Task TestConnection()
         {
             using (IDbConnection conn = Connection)
             {
@@ -27,9 +30,22 @@ namespace TUTORized.Repository
                 conn.Open();
                 var result = await conn.QueryAsync<String>(query);
             }
-                
-
         }
+        
+
+        //Execute Async
+        private async Task ExecuteAsync(string procedureName, object parameters = null)
+        {
+            using (var connection = Connection)
+            {
+                connection.Open();
+
+                await connection.ExecuteAsync(procedureName, parameters, commandType: CommandType.StoredProcedure).ConfigureAwait(false);
+            }
+        }
+
+        //Retrives rows in a table
+        
 
     }
 }
