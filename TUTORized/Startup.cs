6fe2172142c5d19a65c81b40ec2b5614ca3fafc5
Wes <wpe19d@gmail.com>
2018-10-04 19:28:34 -0500

    fix

--- TUTORized/Startup.cs
+++ TUTORized/Startup.cs
@@ -20,7 +20,7 @@ namespace TUTORized
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
