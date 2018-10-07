60cb9178d301cb94d37bba4873f010c97ce978b5
TimothyMcWatters <timmch2o@gmail.com>
2018-10-06 22:50:59 -0500

    Began unit testing on UserRepository

---
+++ TUTORizedTests/UserRepositoryTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using TUTORized;
+
+namespace TUTORizedTests
+{
+    public class UserRepositoryTests
+    {
+
+
+
+        [Fact]
+        public void TestAbleToCreateUserProfile()
+        {
+            //ARRANGE
+            var sut = new UserRepository();
+            var user = new User("UserRepositoryTest@test.com", "userRepositoryTestPassword", "userRepositoryTestFirstName",
+                "userRepositoryTestLastName", "userRepositoryTestRole");
+
+            //ACT
+            sut.UserProfileCreateAsync(user);
+            User retrievedUser = GetUserByEmailAsync("UserRepositoryTest@test.com");
+
+            //ASSERT
+            Assert.Equals(user, retrievedUser);
+        }
+    }
+}
+
+
+Task UserProfileCreateAsync(User user);
+
+Task<User> GetUserByEmailAsync(string email);
+
+Task<User> GetUserByIdAsync(string Id);
+
+Task UserProfileUpdateAsync(User user);
+
+Task UserProfileDeleteAsync(string Id);
