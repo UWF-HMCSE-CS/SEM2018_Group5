844531bb766ff1a68eaeb88cf934a277cb6b008f
TimothyMcWatters <timmch2o@gmail.com>
2018-10-19 11:12:37 -0500

    Coded rough draft of Student (repository, service, controller and thier interfaces).

---
+++ TUTORized/Controllers/StudentController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TUTORized.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Student")]
+    public class StudentController : Controller
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        // GET: api/Student
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Student/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Student
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Student/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TUTORized/Services/StudentService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TUTORized.Models;
+using TUTORized.Services.Abstract;
+
+/**
+TUTORized is a web application designed for use in 
+schools. Students can coordinate with Tutors to set 
+up tutoring sessions. Students and Tutors are also 
+able to message each other, as well as share resources
+with each other.
+@author Timothy McWatters
+@author Keenal Shah
+@author Wesley Easton
+@author Wenwen Xu
+@version 1.0
+CEN4053    "TUTORized" SEM- Group 5's class project
+File Name: ServiceService.cs 
+    This class provides the logic for all Student actions.
+*/
+
+namespace TUTORized.Services
+{
+    public class StudentService : IStudentService
+    {
+        public Task<List<User>> ListOfTutorsGet()
+        {
+            return null;
+        }
+    }
+}
