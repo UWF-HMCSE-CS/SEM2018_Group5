c38456729b441c93c93d4eb4660c68b355fd6ec7
Wen <wx2@students.uwf.edu>
2018-10-25 18:19:40 -0500

    update ts file

--- TUTORized/ClientApp/components/calendar/calender.ts
+++ TUTORized/ClientApp/components/calendar/calender.ts
@@ -2,34 +2,19 @@
 import { Component } from 'vue-property-decorator';
 import $ from 'jquery';
 import { Appointment } from '../../models/Appointment';
-import { User } from '../../models/User';
-
-
+import studentSrvices from '../../services/student/studentServices';
 
 @Component
 export default class CalenderComponent extends Vue{
 
-    appointment = new Appointment();
-    user = new User();
-    
+    appointments: Array<Appointment> = [];
+ 
+    method()
+    {
+        studentSrvices.getListOfAppointments().then(result =>{
+            this.appointments = result;
 
-    getData(appo: Appointment){
-        $.ajax({
-            type: "GET",
-            url:'api/listAppointments',
-            dataType:'text/json',
-            success: function(){
-                return appo;
-            }
-        });
-
-    this.appointment.id = appo.id;
-    this.appointment.date = appo.date;
-    this.appointment.tutorId = appo.tutorId;
-    this.appointment.duration = appo.duration;
-    this.appointment.subject = appo.subject;
-
-
+        })
     }
-  
+    
 }
