a5776176193065092df351d3d2fb53bac5760547
Keenal <kms118@students.uwf.edu>
2018-10-25 11:08:43 -0500

    add appointment controller and IAppointmentService

---
+++ TUTORized/Controllers/AppointmentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TUTORized.Services.Abstract;
+
+namespace TUTORized.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Appointment")]
+    public class AppointmentController : Controller 
+    {
+
+        private readonly IAppointmentService _appointmentService;
+
+        public AppointmentController(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
+        // GET : api/Appointment/GetListOfAllAppointments
+
+        [HttpGet]
+        public async Task<IActionResult> GetListOfAllAppointment()
+        {
+
+        }
+    }
+}
---
+++ TUTORized/Services/Abstract/IAppointmentService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TUTORized.Services.Abstract
+{
+    public class IAppointmentService
+    {
+    }
+}
