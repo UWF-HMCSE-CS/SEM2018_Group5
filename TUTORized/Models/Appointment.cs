4ab1107de3b3cea51c0d56f0fefeed1eb39c7602
TimothyMcWatters <timmch2o@gmail.com>
2018-10-25 11:04:49 -0500

    added Appointment model

---
+++ TUTORized/Models/Appointment.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TUTORized.Repository;
+
+/**
+TUTORized is a web application designed for use in 
+schools. Students can coordinate with Tutors to set 
+up tutoring sessions. Students and Tutors are also 
+able to message each other, as well as share resources
+with each other.
+@author Timothy McWatters
+@author Keenal Shah
+@author Wesley Easton
+@author Wenwen Xu
+@version 1.0
+CEN4053    "TUTORized" SEM- Group 5's class project
+File Name: Appointment.cs 
+    This class is the User Model. 
+*/
+
+namespace TUTORized.Models
+{
+    public class Appointment
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("studentId")]
+        public string StudentId { get; set; }
+
+        [JsonProperty("tutorId")]
+        public string TutorId { get; set; }
+
+        [JsonProperty("date")]
+        public string Date { get; set; }
+
+        [JsonProperty("duration")]
+        public string Duration { get; set; }
+
+        [JsonProperty("subject")]
+        public string Subject { get; set; }
+    }
+}
