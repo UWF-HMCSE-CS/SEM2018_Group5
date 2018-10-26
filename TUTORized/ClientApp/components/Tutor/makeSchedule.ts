0d1f2c7549bcb20d0365b5f09402e2e4dfeddf85
Wen <wx2@students.uwf.edu>
2018-10-26 17:51:37 -0500

    last modify layout, maybe

--- TUTORized/ClientApp/components/Tutor/makeSchedule.ts
+++ TUTORized/ClientApp/components/Tutor/makeSchedule.ts
@@ -6,11 +6,38 @@ import { Appointment } from '../../models/Appointment';
 @Component
 export default class SignUpComponent extends Vue {
 
+    appointment = new Appointment();
+    id: string = '';
+    studentId: string = '';
+    tutorId: string = '';
     date: string = '';
     duration: string = '';
     subject: string = '';
-   
-    submitFunction() {
+    studentFirstName: string = '';
+    studentLastName: string = '';
+    tutorFirstName: string = '';
+    tutorLastName: string = '';
+    
+    //set subject array
+    subjects: any[] = [
+        {subject: 'Math'},
+        {subject: 'CS'},
+        {subject: 'Science'},
+        {subject: 'History'},
+        {subject: 'English'}
+    ];
+
+    //set duration array
+    durations: any[] = [
+        {duration: 1},
+        {duration: 2},
+        {duration: 3},
+        {duration: 4},
+        {duration: 5},
+        {duration: 6},
+    ];
+
+    signUpFunction() {
 
         $.ajax({
             headers: {
@@ -18,14 +45,25 @@ export default class SignUpComponent extends Vue {
                 'Content-Type': 'application/json'
             },
             type: "POST",
-            url: 'api/user/makeSchedule',
+            url: 'api/user/registerUser',
             data: JSON.stringify({
-               
-                
+                "Id": this.appointment.id,
+                "StudentId": this.appointment.studentId,
+                "TutorId": this.appointment.tutorId,
+                "Date": this.appointment.date,
+                "Duration": this.appointment.duration,
+                "Subject": this.appointment.subject,
+                "StudentFirstName": this.appointment.studentFirstName,
+                "StudentLastName": this.appointment.studentLastName,
+                "TutorFirstName": this.appointment.tutorFirstName,
+                "TutorLastName": this.appointment.tutorLastName
             }),
             dataType: 'json',
             complete: function (response) {
-                alert("Scheduled Successfully");
+
+                alert('Scheduled Successfully');
+                window.location.href = "/makeSchedule";
+                
             }
         });
     }
