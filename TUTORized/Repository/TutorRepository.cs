25bbb8ff098040f9f6664aca05d452600dde738a
Wes Easton <wpe19d@gmail.com>
2018-10-26 15:05:13 -0500

    quick fix for logged in user

--- TUTORized/Repository/TutorRepository.cs
+++ TUTORized/Repository/TutorRepository.cs
@@ -49,10 +49,8 @@ namespace TUTORized.Repository
             parameters.Add("Date", appointment.Date);
             parameters.Add("Duration", appointment.Duration);
             parameters.Add("Subject", appointment.Subject);
-            //parameters.Add("studentFirstName", appointment.StudentFirstName);
-            //parameters.Add("studentLastName", appointment.StudentLastName);
-            //parameters.Add("tutorFirstName", appointment.TutorFirstName);
-            //parameters.Add("tutorLastName", appointment.TutorLastName);
+            parameters.Add("TutorFirstName", appointment.TutorFirstName);
+            parameters.Add("TutorLastName", appointment.TutorLastName);
 
             var result = await FirstJsonResultAsync<Appointment>("createAppointment", parameters);
             return result;
