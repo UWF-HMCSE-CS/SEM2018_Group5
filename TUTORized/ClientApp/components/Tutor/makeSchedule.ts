77f8b9a17fb6a9c4bc639c06b048d479846f921b
Wen <wx2@students.uwf.edu>
2018-10-26 18:20:57 -0500

    500 error

--- TUTORized/ClientApp/components/Tutor/makeSchedule.ts
+++ TUTORized/ClientApp/components/Tutor/makeSchedule.ts
@@ -7,17 +7,7 @@ import { Appointment } from '../../models/Appointment';
 export default class SignUpComponent extends Vue {
 
     appointment = new Appointment();
-    id: string = '';
-    studentId: string = '';
-    tutorId: string = '';
-    date: string = '';
-    duration: string = '';
-    subject: string = '';
-    studentFirstName: string = '';
-    studentLastName: string = '';
-    tutorFirstName: string = '';
-    tutorLastName: string = '';
-    
+        
     //set subject array
     subjects: any[] = [
         {subject: 'Math'},
@@ -37,7 +27,7 @@ export default class SignUpComponent extends Vue {
         {duration: 6},
     ];
 
-    signUpFunction() {
+    submitFunction() {
 
         $.ajax({
             headers: {
@@ -45,24 +35,21 @@ export default class SignUpComponent extends Vue {
                 'Content-Type': 'application/json'
             },
             type: "POST",
-            url: 'api/user/registerUser',
+            url: 'api/tutor/createAppointment',
             data: JSON.stringify({
                 "Id": this.appointment.id,
-                "StudentId": this.appointment.studentId,
                 "TutorId": this.appointment.tutorId,
                 "Date": this.appointment.date,
                 "Duration": this.appointment.duration,
                 "Subject": this.appointment.subject,
-                "StudentFirstName": this.appointment.studentFirstName,
-                "StudentLastName": this.appointment.studentLastName,
                 "TutorFirstName": this.appointment.tutorFirstName,
                 "TutorLastName": this.appointment.tutorLastName
             }),
             dataType: 'json',
             complete: function (response) {
 
-                alert('Scheduled Successfully');
-                window.location.href = "/makeSchedule";
+                //alert('Scheduled Successfully');
+                //window.location.href = "/makeSchedule";
                 
             }
         });
