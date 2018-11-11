342a12ac9e96b6b458210b0dc585ca66944c92da
Keenal <kms118@students.uwf.edu>
2018-11-11 00:00:19 -0600

    add client side chat

--- TUTORized/ClientApp/boot.ts
+++ TUTORized/ClientApp/boot.ts
@@ -15,7 +15,7 @@ const routes = [
     { path: '/scheduleappointment', component: require('./components/student/scheduleAppointment.vue.html').default },
     { path: '/makeSchedule', component: require('./components/Tutor/makeSchedule.vue.html').default },
 
-  //  { path: '/chat', component: require('./components/chat/chat.vue.html').default }
+    { path: '/chat', component: require('./components/chat/chat.vue.html').default },
 
     { path: '/ListOfUsersWorkedWith', component: require('./components/UserWorkedWith/ListOfUsersWorkedWith.vue.html').default }
 
